e325d1100230da9c707bf85b3584279d8ceaf912
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-29 15:36:34 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./config/env";
+
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+    console.log("connected to DB ✅");
+
+    server = app.listen(5000, () => {
+      console.log(`Server is listening to port ${envVars.PORT} `);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected...... Server shutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("unCaughtException", (err) => {
+  console.log("uncaught Exception detected...... Server shutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received...... Server shutting down..");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
